714b765dab77c11dacb523d805cfbe014f342f7e
BlackLineMkt <contato.blacklineagency@gmail.com>
2026-05-02 23:02:01 -0300

    Update leadsClient.ts

--- src/lib/leadsClient.ts
+++ src/lib/leadsClient.ts
@@ -7,4 +7,4 @@ export const leadsSupabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6YmZ2ZXN6anVteHNodWF0enpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxNTExODMsImV4cCI6MjA5MjcyNzE4M30.pVCzQ59dG7K4gvdrDroZH1LLxZ0XqtqxOc9uH0gPPoU",
 );
 
-export const LEADS_STUDIO_ID = "71d45bda-cda5-4b61-9d90-108bb16ad079";
+export const LEADS_STUDIO_ID = "c7f9ab76-f223-4b04-8fb6-fd63fee979b5";
